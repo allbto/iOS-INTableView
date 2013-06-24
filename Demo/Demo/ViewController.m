379ebe6e02925a51048fa0848f4069e6f204d1e0
Allan Barbato <barbat_a@epitech.eu>
2013-06-24 15:20:46 +0200

    Screenshot + Input Cell Title

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -26,11 +26,11 @@
     // This is the old way, I used it because I had some problem with XIB file and INTableView, but it's fixed now
     
     // By calling this method you allow the tableView to have a pullToRefreshView and call the block when the user reloads it
-    [self.tableView setPullToRefresh:YES withBlock:^(INTableView *table) {
+    [self.tableView setPullToRefresh:YES withBlock:^(INTableView *tableView) {
         NSLog(@"Did Pull to Refresh");
         
         // Once you're over with you download and other don't forget to call :
-        self.tableView.pullToRefreshLoading = NO;
+        tableView.loading = NO;
     }];
     
     //
@@ -41,8 +41,8 @@
     }]];
     [self.tableView addCell:[INTableViewInputCell inputCellWithTitle:@"Title" prompt:@"Prompt"]];
     //[self.tableView addCell:<#(INTableViewCell *)#>]
-    [self.tableView addCell:[INTableViewLoadingCell loadingCell]];
     [self.tableView addCell:[INTableViewSwitchCell defaultCell]];
+    [self.tableView addCell:[INTableViewLoadingCell loadingCell]];
     //INTableViewTextCell* textCell = [INTableViewTextCell textCellWithText:@"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum eros id nunc iaculis ultricies. Aenean at purus tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque sit amet arcu nisi. Donec aliquam diam non lectus feugiat a ultrices tellus hendrerit. Curabitur tincidunt interdum arcu ut porttitor. Mauris eget ante sapien, at eleifend lorem. Nunc sem risus, tempus ac sodales vel, vulputate ac lacus. Sed eget lacus orci. Phasellus quis felis quis erat scelerisque ultricies quis vel turpis. Donec sapien arcu, pharetra vel posuere vitae, malesuada eu sem. Integer nisl urna, rutrum ut sodales at, convallis a felis." editable:YES];
     //textCell.expendWhenTextChanges = YES;
     //[self.tableView addCell:textCell];
